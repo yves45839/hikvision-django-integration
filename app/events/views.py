f9b2e26fcb2063d5521e76e1ab8d3bbd0cbb1057
yves45839 <ouiyayves@gmail.com>
2026-02-25 20:59:33 +0000

    Restrict events API list to authenticated user's devices

--- app/events/views.py
+++ app/events/views.py
@@ -3,5 +3,8 @@ from .models import AttendanceEvent
 from .serializers import AttendanceEventSerializer
 
 class AttendanceEventViewSet(viewsets.ModelViewSet):
-    queryset = AttendanceEvent.objects.all().order_by('-id')
+    queryset = AttendanceEvent.objects.none()
     serializer_class = AttendanceEventSerializer
+
+    def get_queryset(self):
+        return AttendanceEvent.objects.filter(device__owner=self.request.user).order_by('-id')
